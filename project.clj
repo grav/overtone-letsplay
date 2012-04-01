41db60687f8fcf090ba3eb954e18c8be4c339fd4
Mikkel Gravgaard <mikkel@klokke.dk>
2012-04-01 14:48:17 +0200

    pattern matching dep

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject letsplay "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"]
                  [overtone "0.6.0"]])

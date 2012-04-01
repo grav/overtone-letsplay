8ffee137072d0f7df9002da902e242511fffd9d5
Mikkel Gravgaard <mikkel@klokke.dk>
2012-04-01 14:50:28 +0200

    dynamic binding

--- src/letsplay/core.clj
+++ src/letsplay/core.clj
@@ -7,8 +7,7 @@
        env (env-gen (perc 0.01 0.3) :action FREE)]
     (* src env)))
 
-;; one way of
-
+;; one way of enabling dynamic binding
 (defn my-play [time tones sep]
   (let [tone (first tones)]
     (at time
@@ -16,38 +15,15 @@
     (let [next-time (+ time sep)]
       (apply-at next-time my-play next-time (rest tones) sep []))))
 
-(defmacro loop-play [time tones sep]
-  (list 'my-play time (list 'flatten (list 'repeatedly (list 'fn [] tones))) sep))
+(def tones)
 
-;; (def tones [65 66 67]
-;; (loop-play (now) tones 200)
+(defn loop-play [time tones sep]
+  (my-play time (flatten (repeatedly (fn [] (deref #'tones)))) 200))
+
+;; (def tones [65 66 67])
+;; (loop-play (now) tones 200)0
 ;; redefining tones will work, ie
 ;; (def tones [67 66 65]
-
-;; another way
-(defn tonesfn []
-  '(73 68 71))
-
-(defn my-play2 [time tones sep])
-
-;; (my-play2 (now) (flatten (repeatedly #'tonesfn)) 200)
-;; redefining tonesfn will work, ie
-;; (defn tonesfn [] '(71 68 72))
-
-
-
-(for [n '(65 67 71)] (beep n))
-
-
-
-
-(defn myplay [time notes sep]
-  (let [note (first notes)]
-    (when note
-      (at time
-          (beep note))
-      (let [next-time (+ time sep)]
-        (apply-at next-time myplay [next-time (rest notes) sep])))))
 
 (defn updown [tones]
   (concat tones  (reverse (drop 1 (take (dec (count tones)) tones)))))

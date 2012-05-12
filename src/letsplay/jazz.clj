d360ad789e997016e806c91a6512774c0a56fa1e
Mikkel Gravgaard <mikkel@klokke.dk>
2012-05-12 11:02:59 +0200

    getting a grip on the namespace thing

--- src/letsplay/jazz.clj
+++ src/letsplay/jazz.clj
@@ -1,10 +1,8 @@
-(ns betafunk.jazz
+(ns letsplay.jazz
   (:use [overtone.live]
         [overtone.inst.drum]
-        [overtone.inst.synth]))
-
-;; a bit of pattern matching
-(use '[clojure.core.match :only [match]])
+        [overtone.inst.synth]
+        [letsplay.rotater]))
 
 ;; just a simple example of a synth
 ;; we'll use this together with the bass
@@ -13,48 +11,6 @@
       [src (sin-osc (midicps note))
        env (env-gen (perc 0.01 0.9) :action FREE)]
     (* src env)))
-
-;; Specify input device
-;; No arg will list midi devices in pop-up and
-;; allow you to select one
-(def kb (midi-in "FastTrack Pro"))
-
-;; Specify output device
-(def synth-out (midi-out "FastTrack Pro"))
-
-;; Rotate between these notes
-(def rotate (ref '(-10 -7 -14 -5)) )
-
-;; TODO - use a pointer into the list mod list length instead
-(defn next-rotate []
-  (let [note (first @rotate)]
-    (ref-set rotate (concat (rest @rotate) (list note)))
-    note))
-
-
-;; Init a vector of 128 empty lists
-(def notes-playing
-  (ref (vec (repeat 128 '()))))
-
-(defn add-notes [note notes]
-  (ref-set notes-playing
-           (assoc @notes-playing note notes)))
-
-;; the rotater function to handle incoming midi
-(defn rotater [event ts]
-  (let [chan (:chan event)
-        cmd (:cmd event)
-        note (:note event)]
-    (match [chan cmd]
-      ;; note-on
-      [_ 144] (dosync ;; (next-rotate) and (add-notes) must be sync'ed
-               (let [notes (map #(+ % note) [(next-rotate) 0 7])]
-                 (add-notes note notes) ;; mapping note => notes
-                 (doseq [n notes] (midi-note-on synth-out n (:vel event)))))
-      ;; note-off
-      [_ 128] (let [notes (@notes-playing note)]
-                (doseq [n notes] (midi-note-off synth-out n))))))
-
 
 ;; drums
 (def ride (sample (freesound-path 436)))
@@ -136,7 +92,13 @@
 (def tempo 160)
 (def metro (metronome tempo))
 
-;; Place cursor at the end of these expressions and do C-x e to execute them
+;; Specify input device
+;; No arg will list midi devices in pop-up and
+;; allow you to select one
+(def kb (midi-in "FastTrack Pro"))
+
+;; Place cursor at the end of these expressions
+;; and do C-x e to execute them
 
 ;; Set up rotater
 ;; (midi-handle-events kb #'rotater)
--- src/letsplay/rotater.clj
+++ src/letsplay/rotater.clj
@@ -5,16 +5,12 @@
 ;; http://youtu.be/4kBpxBJkknY ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(ns letsmidi
-  (:use overtone.live))
+(ns letsplay.rotater
+  (:use [overtone.live]))
 
 ;; a bit of pattern matching
 (use '[clojure.core.match :only [match]])
 
-;; Specify input device
-;; No arg will list midi devices in pop-up and
-;; allow you to select one
-(def kb (midi-in "FastTrack Pro"))
 
 ;; Specify output device
 (def synth-out (midi-out "FastTrack Pro"))
@@ -22,10 +18,12 @@
 ;; Rotate between these notes
 (def rotate (ref '(-10 -7 -14 -5)) )
 
+;; TODO - use a pointer into the list mod list length instead
 (defn next-rotate []
   (let [note (first @rotate)]
     (ref-set rotate (concat (rest @rotate) (list note)))
     note))
+
 
 ;; Init a vector of 128 empty lists
 (def notes-playing
@@ -35,20 +33,17 @@
   (ref-set notes-playing
            (assoc @notes-playing note notes)))
 
-(defn expander [event ts]
+;; the rotater function to handle incoming midi
+(defn rotater [event ts]
   (let [chan (:chan event)
         cmd (:cmd event)
         note (:note event)]
     (match [chan cmd]
-      [14 _] nil ;; filter out active sense
+      ;; note-on
       [_ 144] (dosync ;; (next-rotate) and (add-notes) must be sync'ed
                (let [notes (map #(+ % note) [(next-rotate) 0 7])]
                  (add-notes note notes) ;; mapping note => notes
-                 (doall
-                  (map #(midi-note-on synth-out % (:vel event)) notes))))
+                 (doseq [n notes] (midi-note-on synth-out n (:vel event)))))
+      ;; note-off
       [_ 128] (let [notes (@notes-playing note)]
-                (doall
-                 (map #(midi-note-off synth-out %) notes))))))
-
-;; Set the expander function to handle incoming midi
-;; (midi-handle-events kb #'expander)
+                (doseq [n notes] (midi-note-off synth-out n))))))

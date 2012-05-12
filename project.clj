80bcac2534ea433aef70e072b550d1741e9e58fd
Mikkel Gravgaard <mikkel@klokke.dk>
2012-05-12 10:47:11 +0200

    project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject letsplay "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Some Overtone experiments"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]
                  [overtone "0.6.0"]])

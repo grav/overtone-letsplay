7801c3d0a0823dc2ad894fbc8324a71039eaaaac
Mikkel Gravgaard <mikkel@klokke.dk>
2012-05-29 00:27:24 +0200

    template

---
+++ src/letsplay/jam-template.clj
@@ -0,0 +1,39 @@
+(ns letsplay.jam
+  (:use [overtone.live]
+        [overtone.inst.drum]))
+
+;; this loads a sample into a buffer
+(defn fs-buffer [q n]
+  (let [result (freesound-search :q q :f "duration:[0.0 TO 1.0]")
+        id (:id (nth result n))]
+    (load-sample (freesound-path id))))
+
+;; this makes a quick play buffer with a freesound in it!
+(defn quickfree
+  ([query n] (quickfree query n 1))
+  ([query n rate]
+     (play-buf 1 (fs-buffer query n) rate)))
+
+(definst noise1 [vol 1]  (* vol (quickfree "ding" 1 2)))
+(definst noise2 [vol 1]  (* vol (quickfree "ding" 2 1.5)))
+(definst noise3 [vol 1]  (* vol (quickfree "ding" 3)))
+(definst noise4 [vol 1]  (* vol (quickfree "ding" 7 1)))
+
+(def noises (list noise1 noise2 noise3 noise4))
+
+((rand-nth noises))
+
+;; initial beep
+(definst beep [note 60]
+  (let
+      [src (sin-osc (midicps note))
+       env (env-gen (perc 0.01 0.3) :action FREE)]
+    (* src env)))
+
+
+(def tones)
+
+(def metro (metronome 160))
+
+;; (drumbeat (metro) 0)
+;; (my-play (metro) (flatten (repeatedly (fn [] tones))))

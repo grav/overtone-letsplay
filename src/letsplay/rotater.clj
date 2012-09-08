fa29e4307223f2f74b2612c3c2cb1328ca51c1d7
Mikkel Gravgaard <mikkel@klokke.dk>
2012-09-08 17:22:03 +0200

    factor out note-on and note-off functions to allow easier call without midi events

--- src/letsplay/rotater.clj
+++ src/letsplay/rotater.clj
@@ -33,21 +33,29 @@
   (ref-set notes-playing
            (assoc @notes-playing note notes)))
 
+(defn rotater-on [note vel]
+  (dosync ;; (next-rotate) and (add-notes) must be sync'ed
+   (let [notes (map #(+ % note) [(next-rotate) 0 7])]
+     (prn 'on notes)
+     (add-notes note notes) ;; mapping note => notes
+     (doseq [n notes] (midi-note-on synth-out n vel))
+     )))
+
+(defn rotater-off [note]
+  (let [notes (@notes-playing note)]
+    (prn 'off notes)
+    (doseq [n notes] (midi-note-off synth-out n))
+    ))
+
 ;; the rotater function to handle incoming midi
 (defn rotater [event ts]
   (let [chan (:chan event)
         cmd (:command event)
-        note (:note event)]
+        note (:note event)
+        vel (:velocity event)]
     (match [chan cmd]
       ;; note-on
-      [_ :note-on] (dosync ;; (next-rotate) and (add-notes) must be sync'ed
-               (let [notes (map #(+ % note) [(next-rotate) 0 7])]
-                 (prn 'on notes)
-                 (add-notes note notes) ;; mapping note => notes
-                 (doseq [n notes] (midi-note-on synth-out n (:velocity event)))
-                 ))
+      [_ :note-on] (rotater-on note vel)
+
       ;; note-off
-      [_ :note-off] (let [notes (@notes-playing note)]
-                      (prn 'off notes)
-                      (doseq [n notes] (midi-note-off synth-out n))
-                      ))))
+      [_ :note-off] (rotater-off note))))

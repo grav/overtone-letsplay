4135b2c0840b92f282bc7f2f887c5609b20dc215
Mikkel Gravgaard <mikkel@klokke.dk>
2012-09-10 21:23:55 +0200

    remove dependency on clojure.core.match

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject letsplay "1.0.0-SNAPSHOT"
   :description "Some Overtone experiments"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.match "0.2.0-alpha9"]
-                 ;;[overtone "0.6.0"]
                  [overtone "0.7.1"]])
